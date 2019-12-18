25d139b46f2ea0e159312dab9a828c27445515e8
Narendra Kumar <naren@Narendras-MBP.lan>
2019-12-18 00:12:03 -0800

    added sidebar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import Sidebar from './sidebar'
 import '../styles/index.scss'
 import layoutStyles from './layout.module.scss'
+import sidebar from './sidebar'
 const Layout = (props) => {
     return (
         
         <div className={layoutStyles.container}>
             <Header />
-            {props.children}
-            <Footer />
+            <main style={{width: `70%`, float: `left`}}>
+            {props.children}</main>
+            <Sidebar />
         </div>
     )
 }
---
+++ src/components/sidebar.js
@@ -0,0 +1,7 @@
+import React from "react"
+export default () => (
+    <aside style={{marginTop: `3.2rem`, padding: `10px `}}>
+        <h3> Sidebar</h3>
+        <p> hooray</p>
+    </aside>
+)
